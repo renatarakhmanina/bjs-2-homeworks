debaa04b9aa7dad74274dd3d33e92886644cb776
Renata Rakhmanina <renatarahmanina@gmail.com>
2023-07-25 14:47:37 +0300

    Both tasks are finished

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2) - (4 * a * c);
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonthly = percent / 100 / 12;
+  let loanBody = amount - contribution;
+  let paymentMonthly = loanBody * (percentMonthly + (percentMonthly / (Math.pow((1 + percentMonthly), countMonths) - 1)));
+  let totalMortage = +(paymentMonthly * countMonths).toFixed(2);
+  return totalMortage;
 }
