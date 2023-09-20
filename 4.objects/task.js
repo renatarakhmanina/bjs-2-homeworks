a6ce3dc45d6213a77fe868d07c51b884cf279ef4
Renata Rakhmanina <renatarahmanina@gmail.com>
2023-09-20 03:39:30 +0200

    task.js changed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,6 +4,7 @@ function Student(name, gender, age) {
   this.age = age;
   this.marks = [];
 }
+
 let student1 = new Student('Петр Петров', 'male', 20);
 let student2 = new Student('Анна Лукьянова', 'female', 22);
 let student3 = new Student('Николай Павлов', 'male', 19);
@@ -24,13 +25,7 @@ Student.prototype.getAverage = function () {
   if (!this.hasOwnProperty('marks') || this.marks.length === 0) {
     return 0;
   } else {
-    this.marks.reduce((acc, item, index) => {
-      acc += item;
-      if (index === this.marks.length - 1) {
-        return acc / this.marks.length;
-      }
-      return acc;
-    }, 0)
+    return this.marks.reduce((acc, item) => acc + item , 0) / this.marks.length;
   }
 }
 
